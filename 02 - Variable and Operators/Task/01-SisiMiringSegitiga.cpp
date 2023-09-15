0a8e6bef38150bd5e4b91f1606290ae7c878aacd
rifkibayuariyan <bayuariyan08@gmail.com>
2023-09-15 18:58:51 +0700

    Upload Task Pythagoras version 1

---
+++ 02 - Variable and Operators/Task/01-SisiMiringSegitiga.cpp
@@ -0,0 +1,60 @@
+/*
+ * Pythagoras
+ * Author: Rifki Bayu Ariyanto
+ *
+ * Mencari nilai sisi miring segitiga
+ */
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    // Variable Declation
+    float miring;
+    map<string, int> segitiga{{"Alas", 0}, {"Tinggi", 0}};
+
+    // Display information about program
+    cout << "+------------------------------------------------+" << endl;
+    cout << "|  [] Pythagoras Calculator                      |" << endl;
+    cout << "+------------------------------------------------+" << endl;
+    cout << "|  Program ini digunakan untuk menghitung nilai  |" << endl;
+    cout << "|  sisi miring segitiga                          |" << endl;
+    cout << "+------------------------------------------------+" << endl;
+    cout << "Silahkan masukan nilai" << endl
+         << endl;
+
+    // Console Input Alas and Tinggi
+    map<string, int>::iterator i;
+    for (i = segitiga.begin(); i != segitiga.end(); i++)
+    {
+        int counter = 0;
+        do
+        {
+            if (counter > 0)
+                cout << "Mohon masukan angka lebih dari 0!" << endl;
+
+            cout << "Nilai Sisi " << i->first << " : ", cin >> segitiga[i->first];
+            counter++;
+        } while (!isdigit(segitiga[i->first]) && segitiga[i->first] <= 0);
+    }
+
+    // Calculate to find Miring Value
+    miring = sqrt((pow(segitiga["Tinggi"], 2)) + (pow(segitiga["Alas"], 2)));
+
+    cout << endl
+         << "\t=> Result (Sisi Miring)" << endl;
+    cout << "+-----------------------------------------------+" << endl;
+    cout << "\tSisi Tinggi\t: " << segitiga["Tinggi"] << endl;
+    cout << "\tSisi Alas\t: " << segitiga["Alas"] << endl;
+    cout << "\tSisi Miring\t: " << miring << endl;
+    cout << "+-----------------------------------------------+" << endl;
+    cout << "Jadi Sisi Miring dari segitiga adalah " << miring << endl
+         << endl;
+
+    return 0;
+}
