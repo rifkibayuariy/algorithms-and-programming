c4c40502d46780372a301abbcf063d390c37bf39
rifkibayuariyan <bayuariyan08@gmail.com>
2023-10-05 11:21:23 +0700

    Add description

--- 05 - Looping/Lab/Task/05-LoopingTask-FactorialOfNumber.cpp
+++ 05 - Looping/Lab/Task/05-LoopingTask-FactorialOfNumber.cpp
@@ -12,19 +12,23 @@ int main() {
     int number;
     long long int factorial = 1;
 
-    cout << "Please input a number : ", cin >> number;
+    cout << "\n\t+---------------------------+" << endl;
+    cout << "\t|    Factorial of Number    |" << endl;
+    cout << "\t+---------------------------+" << endl;
+    cout << "\tPlease input a number : ", cin >> number;
 
     bool positive = number >= 0 ? true : false;
     for (int i = 1; i <= number; i++)
         factorial *= i;
 
-    cout << number << "!: ";
+    cout << "\t+---------------------------+" << endl;
+    cout << "\t" << number << "!: ";
     if (positive)
         cout << factorial << endl;
     else {
         cout << "Not defined for negative numbers" << endl;
     }
 
-    system("pause");
+    cout << "\n\t", system("pause");
     return 0;
 }
